001d338758368ce87c7664a078e92c52bb63e243
seyedarashfatemi-alt <seyed.arash.fatemi@gmail.com>
2026-01-29 18:10:33 +1100

    Em

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,6 +5,9 @@ const ALLOWED_EMAILS = [
   'arash@clarityconsult.com.au',
   'trent@clarityconsult.com.au',
   'pedram@clarityconsult.com.au',
+  'ash.bailey@vida.vic.gov.au',
+  'cameron.grant@vida.vic.gov.au',
+  'stephanie.liu@vida.vic.gov.au',
   // Add your allowed email addresses here
 ];
 
